12374962ee8b6d6ce3750c700a1bfee7e2c7198b
zhuyanyun <yanyunzhu@newbanker.cn>
2019-09-23 20:41:46 +0800

    报错没有解决

---
+++ src/main/java/com/example/study/hystrix/HystrixConfig.java
@@ -0,0 +1,28 @@
+package com.example.study.hystrix;
+
+
+import com.netflix.hystrix.contrib.javanica.aop.aspectj.HystrixCommandAspect;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HystrixConfig {
+
+    @Bean
+    public HystrixCommandAspect hystrixCommandAspect() {
+        return new HystrixCommandAspect();
+    }
+
+    /**
+     * 向监控中心Dashboard发送stream消息
+     */
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet() {
+        ServletRegistrationBean registrationBean =
+                new ServletRegistrationBean(new HystrixMetricsStreamServlet(),"ssss");
+        registrationBean.addUrlMappings("/hystrix.stream");
+        return registrationBean;
+    }
+}
